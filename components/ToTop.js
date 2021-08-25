1c3d13e35a613e535c1b9da68d903dad6aace4ce
Alison Tahiri <alison.tahiri@gmail.com>
2021-08-25 07:47:24 +0200

    Change to top behavior

--- components/ToTop.js
+++ components/ToTop.js
@@ -1,20 +1,17 @@
 import React from "react";
-import { animateScroll as scroll } from "react-scroll";
+import Link from "next/link";
 
 import { BsChevronDoubleUp } from "react-icons/bs";
 
 const ToTop = () => {
   return (
-    <div
-      data-aos="fade-up"
-      data-aos-offset="-100"
-      data-aos-delay={200}
-      onClick={() => scroll.scrollToTop()}
-    >
-      <div className="p-2 border-2 rounded-full cursor-pointer border-primary">
-        <BsChevronDoubleUp className="text-3xl transition-colors duration-300 md:text-4xl text-primary hover:text-hv" />
+    <Link href="/">
+      <div data-aos="fade-up" data-aos-offset="-100" data-aos-delay={200}>
+        <div className="p-2 border-2 rounded-full cursor-pointer border-primary">
+          <BsChevronDoubleUp className="text-3xl transition-colors duration-300 md:text-4xl text-primary hover:text-hv" />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
